255afdad35c7d81f9ae6e59449655c57481f46dd
seregatipich <63545608+seregatipich@users.noreply.github.com>
2022-06-18 14:28:20 +0200

    Update db_manager.py

--- tk104_cashControlBot/db_manager.py
+++ tk104_cashControlBot/db_manager.py
@@ -4,7 +4,7 @@ import sqlite3
 def first_start_handler():
     conn = sqlite3.connect('bot.db')
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER UNIQUE, userName TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER UNIQUE PRIMARY KEY, userName TEXT)')
     cursor.execute(
         'CREATE TABLE IF NOT EXISTS expenses (exp_id INTEGER UNIQUE PRIMARY KEY, id INTEGER, '
         'date TEXT, amount INTEGER, name TEXT, foreign key (id) references users(id))'
@@ -14,18 +14,23 @@ def first_start_handler():
 
 
 def save_user(user_id):
-    conn = sqlite3.connect('bot.db')
-    cursor = conn.cursor()
-    cursor.execute(f'INSERT INTO users (id) VALUES ({user_id})')
-    conn.commit()
-    cursor.close()
+    try:
+        conn = sqlite3.connect('bot.db')
+        cursor = conn.cursor()
+        cursor.execute(f'INSERT INTO users (id) VALUES ({user_id})')
+        conn.commit()
+        cursor.close()
+    except sqlite3.Error as error:
+        print(error)
+    finally:
+        conn.close()
 
 
-def expense_handler(expense_name: str, expense_date: str, expense_amount: int):
+def expense_handler(expense_name: str, expense_date: str, expense_amount: int, user_id: int):
     conn = sqlite3.connect('bot.db')
     cursor = conn.cursor()
-    sqlite_insert_query = f'INSERT INTO expenses(name, date, amount) ' \
-                          f'VALUES ({expense_name}, {expense_date}, {expense_amount});'
+    sqlite_insert_query = f'INSERT INTO expenses(id, name, date, amount) ' \
+                          f'VALUES ({user_id}, {expense_name}, {expense_date}, {expense_amount});'
     cursor.execute(sqlite_insert_query)
     conn.commit()
     cursor.close()
